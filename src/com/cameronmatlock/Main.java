ac5259832a2139580f6c73c8c1059221594bee30
cccmatlock <67501974+cccmatlock@users.noreply.github.com>
2021-02-08 19:43:39 -0600

    Trying 2nd time

---
+++ src/com/cameronmatlock/Main.java
@@ -0,0 +1,25 @@
+package com.cameronmatlock;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Wall newWallObj = new Wall();
+
+        newWallObj.setWidth(1.125);
+
+        Wall wallObj2 = new Wall(4.0, 2.0);
+
+        System.out.println(wallObj2.getHeight());
+        System.out.println(wallObj2.getWidth());
+        System.out.println(wallObj2.getArea());
+        System.out.println("");
+        wallObj2.setWidth(3.0);
+        System.out.println(wallObj2.getHeight());
+        System.out.println(wallObj2.getWidth());
+        System.out.println(wallObj2.getArea());
+
+
+
+    }
+}
